bbb6740dd20999e137d54056881a8ef3c6c0481a
Matheus Julidori <m.henrique@ges.inatel.br>
2022-07-12 11:18:55 -0300

    Struct

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,5 +1,9 @@
 defmodule ExMon do
 
-  
+  alias ExMon.Player
+
+  def createPlayer(name,move_avg,move_rnd,move_heal) do
+    Player.build(name,move_rnd,move_avg,move_heal)
+  end
 
 end
---
+++ ex_mon/lib/player.ex
@@ -0,0 +1,20 @@
+defmodule ExMon.Player do
+
+  @max_life 100
+  @struct_attributes [:life, :move_rnd, :move_avg, :move_heal, :name]
+
+  @enforce_keys @struct_attributes
+
+  defstruct @struct_attributes
+
+  def build(name,move_rnd,move_avg,move_heal) do
+    %ExMon.Player{
+      life: @max_life,
+      move_avg: move_avg,
+      move_heal: move_heal,
+      move_rnd: move_rnd,
+      name: name
+    }
+  end
+
+end
