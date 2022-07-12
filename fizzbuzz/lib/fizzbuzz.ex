b58e25bbf2e820a05e9fc93d89f2ee5fce9abb2f
Matheus Julidori <m.henrique@ges.inatel.br>
2022-07-12 10:15:00 -0300

    Privando funções e teste unitário

--- fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/lib/fizzbuzz.ex
@@ -6,13 +6,20 @@ defmodule Fizzbuzz do
     |>handle_file_read()
   end
 
-  def handle_file_read({:ok,  result}), do: convert_string_to_int_list(result)
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:ok,  result}), do: {:ok, convert_string_to_int_list(result)}
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
-  def convert_string_to_int_list(string) do
+  defp convert_string_to_int_list(string) do
     string
     |>String.split(",")
     |>Enum.map(&String.to_integer/1)
+    |>Enum.map(&evaluate_numbers/1)
   end
+
+  defp evaluate_numbers(num) when rem(num,3) == 0  and rem(num,5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(num) when rem(num,3) == 0, do: :fizz
+  defp evaluate_numbers(num) when rem(num,5) == 0, do: :buzz
+  defp evaluate_numbers(num), do: num
+
 
 end
--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -1,8 +1,16 @@
 defmodule FizzbuzzTest do
   use ExUnit.Case
-  doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns {:ok, converted_list}" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz, :fizz, :fizzbuzz]}
+      assert Fizzbuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when an invalid file is provided, returns {:error, error_reason}" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert Fizzbuzz.build("numberss.txt") == expected_response
+    end
   end
+
 end
